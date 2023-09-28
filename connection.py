23be9fecf543b9b2786e320c97d2b8c0877d1149
Johannes Hansen <92086260+Joha00@users.noreply.github.com>
2023-09-28 16:50:42 +0200

    I WANT TO GO HOOOOOOOOOOOOOOOOOOMMMMMMMME

--- connection.py
+++ connection.py
@@ -3,14 +3,15 @@ import requests
 from queue import Queue
 
 if __name__ == '__main__':
-    host = "130.225.39.30"
-    port = "8080"
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    host = '130.225.39.30'
+    port = 8080
 
-sock.connect((host, port))
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# Function to send packets to the server
-while not packet.queue.empty():
-    packet = packet.queue.get()
-    # You can serialize the packet in various ways, as simple bytes for instance
-    response = requests.post(host, data=bytes(packet))
+    sock.connect((host, port))
+
+    # Function to send packets to the server
+    while not packet.queue.empty():
+        packet = packet.queue.get()
+        # You can serialize the packet in various ways, as simple bytes for instance
+        response = requests.post(host, data=bytes(packet))
