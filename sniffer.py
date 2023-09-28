94092aac19d9b38b279af16c063f6d11eb40dadc
cactus <cactusllama4@gmail.com>
2023-09-28 13:53:02 +0200

    sniffer basic class in file

---
+++ sniffer.py
@@ -0,0 +1,31 @@
+import pyshark
+
+class Sniffer():
+    '''
+    BASE CLASS
+    PACKET_COUNT = AMOUNT OF PACKETS PER SNIFF
+    INTERFACE = INTERFACE TO MONITOR (DEFAULT IS FIRST INTERFACE WHEN USING tshark -D)
+    MONITOR_MODE = FOR MONITOR MODE (PROMISCUOUS MODE IF NONE)(STANDARD AFTER DEPLOYMENT SHOULD BE MONITOR)
+    CONT_SNIFF = BOOLEAN TO STOP WHILE LOOP SNIFFING (TODO: FIGURE OUT TO NEGATE THIS DURING RUNTIME)
+    Q = QUEUE MANAGER FROM OTHER FILE (TRANSMITS PACKETS TO MACHINE LEARNING MODEL (THIS IS NOT MODULAR OR GOOD PRACTICE TO HARDCODE LIKE THIS))
+    '''
+    def __init__(self, packet_count=1, interface=None, monitor_mode=True, cont_sniff=True, q=None):
+        self.packet_count = packet_count
+        self.interface = interface
+        self.monitor_mode = monitor_mode
+        self.cont_sniff = cont_sniff
+        self.q = q
+
+    def sniff(self):
+        # set up for continuous capturing
+        capture = pyshark.LiveCapture(interface=self.interface)
+
+        while self.cont_sniff:
+            capture.sniff_continuously(packet_count=self.packet_count)
+            # go through sniffed packets and put into queue
+            for packet in capture:
+                self.q.put(packet)
+
+
+#sniffer = Sniffer(interface = 'Wi-Fi', monitor_mode = None)
+#sniffer.sniff()
