d192a4e48a2c23a96394d075e310508b6e5ef00c
Johannes Hansen <92086260+Joha00@users.noreply.github.com>
2023-09-28 16:03:05 +0200

    ...

--- connection.py
+++ connection.py
@@ -1,4 +1,6 @@
 import socket
+import requests
+from queue import Queue
 
 if __name__ == '__main__':
     host = "130.225.39.30"
@@ -6,3 +8,9 @@ if __name__ == '__main__':
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.connect((host, port))
+
+# Function to send packets to the server
+while not packet.queue.empty():
+    packet = packet.queue.get()
+    # You can serialize the packet in various ways, as simple bytes for instance
+    response = requests.post(host, data=bytes(packet))
