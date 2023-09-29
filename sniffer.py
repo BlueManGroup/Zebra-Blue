a6a8d0778dc0fd15f07573326ba951c460faaaa9
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2023-09-29 14:06:07 +0200

    debug

--- sniffer.py
+++ sniffer.py
@@ -31,7 +31,7 @@ class Sniffer():
             print(self.cont_sniff)
             capture.sniff_continuously(packet_count=self.packet_count)
             # go through sniffed packets and put into queue
-            print(capture.__iter__())
+            print(packet for packet in capture)
             for packet in capture:
                 print("sniffer.py queue size:", self.q.qsize())
                 self.q.put(packet.__str__())
