f42422c3845e4f2a6676b300286246fbc98d752f
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2023-09-29 13:37:01 +0200

    test

--- zebrablue.py
+++ zebrablue.py
@@ -8,8 +8,11 @@ if __name__ == '__main__':
     sniffer = Sniffer(q=q_sniff, packet_count=5)
     connection = Connection(q=q_sniff)
     
-    sniff_proc = mp.Process(target=sniffer.sniff).start()
-    conn_proc = mp.Process(target=connection.connect).start()
+    sniff_proc = mp.Process(target=sniffer.sniff)
+    conn_proc = mp.Process(target=connection.connect)
+
+    sniff_proc.start()
+    conn_proc.start()
+
     sniff_proc.join()
     conn_proc.join()
-    
