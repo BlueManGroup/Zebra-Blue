36848d9b1167127f6f7fad354d8e6ad8e7cd20f2
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2023-10-05 10:36:18 +0200

    add selector class for random search

---
+++ selector.py
@@ -0,0 +1,25 @@
+import random
+
+# TODO: IMPLEMENT ML MODEL TO FIRST ANALYSE PACKETS
+# IF PACKET NON-SUS RUN THROUGH RANDOMIZER
+
+class Selector():
+    def __init__(self, q_sniff=None, q_conn=None):
+        self.q_sniff = q_sniff
+        self.q_conn = q_conn
+        self.cont_select = True
+
+    def stop_select(self):
+        self.cont_select = False
+
+    # RENAME WHEN ML MODEL IMPLEMENTED
+    def select(self):
+        random.seed()
+        
+        while cont_select:
+            # fetch packet from q. if q empty wait for packet to be put into q
+            curPacket = self.q_sniff.get(True)
+
+            if random.randint(0, 100) > 95:
+                self.q_conn.put(curPacket)
+            
