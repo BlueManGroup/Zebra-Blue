65c61d7114992cb3d26d227734fa726dc69a2411
CactusLlama <58257787+CactusLlama@users.noreply.github.com>
2023-10-05 12:15:04 +0200

    fix rnd

--- selector.py
+++ selector.py
@@ -19,7 +19,7 @@ class Selector():
         while self.cont_select:
             # fetch packet from q. if q empty wait for packet to be put into q
             curPacket = self.q_sniff.get(True)
-            rnd := random.randint(0, 100)
+            rnd = random.randint(0, 100)
             if rnd > 99:
                 print(rnd)
                 self.q_conn.put(curPacket)
